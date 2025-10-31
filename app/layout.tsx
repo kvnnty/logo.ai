7da9405caa25bba91fc4ba24e8187e0728feb172
sanskar shastri <sanskar@webbuddy.agency>
2025-10-31 18:12:57 +0530

    redirect fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${manrope.variable} font-primary antialiased`}>
           {children}
           <Toaster />
